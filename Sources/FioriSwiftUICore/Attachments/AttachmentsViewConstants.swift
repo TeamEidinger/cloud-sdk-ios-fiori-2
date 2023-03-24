4e610e1a6111aee14306a6ae016203cc6d5b118a
Marco Eidinger <eidingermarco@gmail.com>
2023-03-24 16:29:19 -0700

    Update AttachmentsViewConstants.swift

--- Sources/FioriSwiftUICore/Attachments/AttachmentsViewConstants.swift
+++ Sources/FioriSwiftUICore/Attachments/AttachmentsViewConstants.swift
@@ -6,14 +6,15 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct AttachmentsViewConstants {
-    static let spaceBetweenTitleAndThumbnails: CGFloat = 11.0
+    static let spaceBetweenTitleAndThumbnails: CGFloat = 11
     
-    static let defaultCornerRadius: CGFloat = 16.0
+    static let defaultCornerRadius: CGFloat = 16
     
-    static let thumbnaiWidth: CGFloat = 110.0
-    static let thumbnaiHeight: CGFloat = 110.0
+    static let thumbnaiWidth: CGFloat = 110
+    static let thumbnaiHeight: CGFloat = 110
     
     static let thumbnailIconWidth: CGFloat = 38.0
     static let thumbnailIconHeight: CGFloat = 38.0
